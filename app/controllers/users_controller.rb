e3b2dec4089b4e221e3c41676e2f8312e137857e
Sherri Mitchell <rsamitchell@att.net>
2015-06-18 12:04:02 -0400

    Added parameters to users controllers file.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def register
     passhash = Digest::SHA1.hexdigest(params[:password])
-    @user = User.new(email: params[:email],
+    @user = User.new(user_name: params[:user_name],
+                      first_name: params[:first_name],
+                      last_name: params[:last_name],
+                      email: params[:email],
                      password: passhash)
     if @user.save
       render users.register.json.jbuilder, status: :created
