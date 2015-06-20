016b4519345785577bc1dc7394d48fbeeaa022d2
Rickard Sunden <rickard.sunden@outlook.com>
2015-06-20 13:02:30 -0400

    Fixed bug and added pagination

--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -2,9 +2,8 @@ class ScoreboardController < ApplicationController
 
   def top_scores
     @top_users = User.joins(:guesses).group(:user_name).order("sum_points DESC")
-                  .limit(10).sum(:points)
+                  .page(params[:page]).per(10).sum(:points)
     if @top_users.any?
-      @top_user.each_
       render 'top_scores.json.jbuilder', status: :ok
     else
       render json: { message: "Sorry no scores to display." },
