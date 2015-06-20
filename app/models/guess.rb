647ee936b7bc7082d829143ff049a5ecf14d5b31
Sherri Mitchell <rsamitchell@att.net>
2015-06-20 14:01:02 -0400

    Adding changes to guess controller, model, partial(_guess), new(view). User can create a guess for a post. Guess will update new guess, number of guesses, points.

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,4 +1,14 @@
 class Guess < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :post
+
+	def check_solution
+		if self.post.answer == self.guess
+			self.points = 30
+			self.post.solution = self.id
+			true
+		else
+			false
+		end
+	end
 end
