dfe7e42cce019a91f827fc3bc836fbd17d11063b
Rickard Sunden <rickard.sunden@outlook.com>
2015-06-22 20:02:08 -0400

    Added regex validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,24 @@
 class User < ActiveRecord::Base
 	EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  USER_REGEX = /\A[a-z0-9_-]{1,20}\z/i
+  PASSWORD_REGEX = /\A[a-z0-9_-]{1,20}\z/i
+  NAME_REGEX = /\A[a-z-]{1,30}\z/i
 
   validates :password, :access_token, presence: true
+  # validates :password, format: { with: PASSWORD_REGEX, 
+  #                                 message: "is not a valid password. Only chars, nums, hyphen, underscore, 1-20 chars"}
   validates :email, uniqueness: true, presence: true
   validates :email, format: { with: EMAIL_REGEX,
                               message: "is not a valid email." }
   validates :user_name, uniqueness: true, presence: true
+  validates :user_name, format: { with: USER_REGEX, 
+                                  message: "is not a valid username. Only chars, nums, hyphen, underscore, 1-20 chars"}
   validates :first_name, presence: true
+  validates :first_name, format: { with: NAME_REGEX, 
+                                  message: "is not a valid name. Only chars, hyphen, 1-30 chars"}
   validates :last_name, presence: true
+  validates :last_name, format: { with: NAME_REGEX, 
+                                  message: "is not a valid name. Only chars, hyphen, 1-30 chars"}
 
   before_validation :ensure_access_token
 
