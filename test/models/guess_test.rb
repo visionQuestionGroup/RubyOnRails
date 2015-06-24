c7a31d18a940afe892f9c3a95bc6ca235fddd9d2
Rickard Sunden <rickard.sunden@outlook.com>
2015-06-24 08:39:10 -0400

    added basic tests

--- test/models/guess_test.rb
+++ test/models/guess_test.rb
@@ -4,4 +4,17 @@ class GuessTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "must have a guess present" do
+    guess = Guess.new(post_id: 1, user_id: 1)
+    refute guess.save
+  end
+
+  test "must have a post id" do
+    guess = Guess.new(guess: "hello", user_id: 1)
+    refute guess.save
+    guess = Guess.new(guess: "hello", user_id: 1, post_id: 1)
+    assert guess.save
+  end
+  
 end
